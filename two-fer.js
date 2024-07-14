4a8df8ccd1883682678a7bbf53ad934325f07a72
blaze5445 <blaze5445@tutamail.com>
2024-07-14 18:29:29 +0300

    Add validation

---
+++ two-fer.js
@@ -0,0 +1,12 @@
+//
+// This is only a SKELETON file for the 'Two fer' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const twoFer = (variable1) => {
+  if (variable1=='Alice')return ("One for Alice, one for me.")
+  if (variable1=='Bob')return ("One for Bob, one for me.")
+  return 'One for you, one for me.'; 
+};
+
+
